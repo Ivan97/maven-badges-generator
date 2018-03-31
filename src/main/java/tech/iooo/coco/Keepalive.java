8459f9d743db0929356f01c0ea3c2f8c22e7952a
YangkiZhang <yangkizhang@gmail.com>
2018-03-31 16:18:35 +0800

    [REFACTOR]:[try to keep the service alive]
    Signed-off-by: YangkiZhang <yangkizhang@gmail.com>

--- src/main/java/tech/iooo/coco/Keepalive.java
+++ src/main/java/tech/iooo/coco/Keepalive.java
@@ -1,13 +1,12 @@
 package tech.iooo.coco;
 
 import io.vertx.core.Vertx;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.IOException;
+import java.net.InetAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
-import tech.iooo.coco.configuration.Constants;
 
 /**
  * Created on 2018/3/31 下午1:04
@@ -22,8 +21,16 @@ public class Keepalive implements ApplicationListener<ApplicationPreparedEvent> 
   public void onApplicationEvent(ApplicationPreparedEvent applicationPreparedEvent) {
     Vertx vertx = Vertx.vertx();
     //for the free dynos will go to sleep after 30 minutes of inactivity
-    vertx.setPeriodic(25 * 60 * 1000, click ->
-        logger.info("click:[{}]", LocalDateTime.now()
-            .format(DateTimeFormatter.ofPattern(Constants.DEFAULT_DATETIME_FORMAT))));
+    vertx.setPeriodic(25 * 60 * 1000, click -> {
+          String ipAddress = "www.baidu.com";
+          try {
+            if (logger.isDebugEnabled()) {
+              logger.debug("baidu reachable:{}", InetAddress.getByName(ipAddress).isReachable(5000));
+            }
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+        }
+    );
   }
 }
