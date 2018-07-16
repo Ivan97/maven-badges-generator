9a6bbbf1392385c4c59245a89ef646dddf40c497
YangkiZhang <yangkizhang@gmail.com>
2018-07-16 17:42:24 +0800

    [REFACTOR]:[keepalive]

--- src/main/java/tech/iooo/coco/Keepalive.java
+++ src/main/java/tech/iooo/coco/Keepalive.java
@@ -1,8 +1,8 @@
 package tech.iooo.coco;
 
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.WebClientOptions;
+import java.io.IOException;
+import org.apache.commons.net.telnet.TelnetClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
@@ -20,19 +20,26 @@ public class Keepalive implements ApplicationListener<ApplicationPreparedEvent> 
   @Override
   public void onApplicationEvent(ApplicationPreparedEvent applicationPreparedEvent) {
     Vertx vertx = Vertx.vertx();
+    vertx.setPeriodic(25 * 60 * 1000, click -> checkStatus());
+  }
 
-    WebClientOptions options = new WebClientOptions()
-        .setConnectTimeout(3000)
-        .setUserAgent("iooo.tech/maven-badge-generator").setKeepAlive(false);
-    WebClient webClient = WebClient.create(vertx, options);
-
-    vertx.setPeriodic(25 * 60 * 1000, click ->
-        webClient.get(80, "www.iooo.tech", "/").send(event -> {
-          if (event.succeeded()) {
-            logger.info("statusCode:{}", event.result().statusCode());
-          } else {
-            logger.error("", event.cause());
-          }
-        }));
+  /**
+   * 检测远程服务器是否可以连接
+   */
+  private void checkStatus() {
+    TelnetClient client = new TelnetClient();
+    try {
+      client.setDefaultTimeout(3000);
+      client.connect("www.iooo.tech", 80);
+      logger.info("keepalive");
+    } catch (Exception e) {
+      logger.warn("remote server: [www.iooo.tech] telnet failed");
+    } finally {
+      try {
+        client.disconnect();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
