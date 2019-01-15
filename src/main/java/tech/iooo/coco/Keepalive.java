586ee8f657237c2edb95ff969240c1a28dffd4e2
YangkiZhang <yangkizhang@gmail.com>
2019-01-15 09:56:20 +0800

    [REFACTOR]:[upgrade]

--- src/main/java/tech/iooo/coco/Keepalive.java
+++ src/main/java/tech/iooo/coco/Keepalive.java
@@ -1,12 +1,14 @@
 package tech.iooo.coco;
 
 import io.vertx.core.Vertx;
-import java.io.IOException;
-import org.apache.commons.net.telnet.TelnetClient;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationPreparedEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
 
 /**
  * Created on 2018/3/31 下午1:04
@@ -17,29 +19,31 @@ public class Keepalive implements ApplicationListener<ApplicationPreparedEvent> 
 
   private static final Logger logger = LoggerFactory.getLogger(Keepalive.class);
 
+  private WebClient client;
+
   @Override
-  public void onApplicationEvent(ApplicationPreparedEvent applicationPreparedEvent) {
+  public void onApplicationEvent(@NonNull ApplicationPreparedEvent applicationPreparedEvent) {
     Vertx vertx = Vertx.vertx();
+    this.client = WebClient.create(vertx);
     vertx.setPeriodic(25 * 60 * 1000, click -> checkStatus());
   }
 
   /**
-   * 检测远程服务器是否可以连接
+   * keepalive
    */
   private void checkStatus() {
-    TelnetClient client = new TelnetClient();
-    try {
-      client.setDefaultTimeout(3000);
-      client.connect("www.iooo.tech", 80);
-      logger.info("keepalive");
-    } catch (Exception e) {
-      logger.warn("remote server: [www.iooo.tech] telnet failed");
-    } finally {
-      try {
-        client.disconnect();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
+    client
+        .get(443, "coco.iooo.tech", "/")
+        .ssl(true)
+        .send(ar -> {
+          if (ar.succeeded()) {
+            HttpResponse<Buffer> response = ar.result();
+            if (logger.isTraceEnabled()) {
+              logger.trace("Received response with status code {}", response.statusCode());
+            }
+          } else {
+            logger.error("Something went wrong {}", ar.cause().getMessage());
+          }
+        });
   }
 }
